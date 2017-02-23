84b9c00203f950c57feb2260f1468b1f74e1b26c
zhaochenguang <kevin1993@sildev.cn>
2017-02-23 15:14:17 +0800

    init basic app structure

---
+++ src/components/BatchSelect.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './stylesheet/BatchSelect.css'
+
+class BatchSelect extends Component {
+    constructor(props) { 
+        super(props)
+    }
+    handleChange(ids, event) { }
+    render() {
+        return (
+            <div></div>
+        )
+    }
+
+}
+export default BatchSelect
